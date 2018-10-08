51473b00ec7297d00e990e622ed6805be699df65
Max Gonzih <gonzih@gmail.com>
2018-10-07 21:44:49 -0400

    Wip

--- wasm-go-toronto/server.go
+++ wasm-go-toronto/server.go
@@ -7,12 +7,12 @@ import (
 
 func wasmHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/wasm")
-	http.ServeFile(w, r, "example.wasm")
+	http.ServeFile(w, r, "test.wasm")
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.HandleFunc("/example.wasm", wasmHandler)
+	mux.HandleFunc("/test.wasm", wasmHandler)
 	log.Fatal(http.ListenAndServe(":3000", mux))
 }
